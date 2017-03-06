98ea5898b48d36ee8cdc052c16b8bd22f0316b7e
Julian- <Julian-@Julian>
2017-03-06 13:06:29 -0500

    TODOS LOS EJERCICIOS DE KAREL

---
+++ src/Ejercicio7/MainCuartosLimpios.java
@@ -0,0 +1,12 @@
+package Ejercicio7;
+
+
+
+public class MainCuartosLimpios {
+       
+    public static void main(String[] args){
+        CuartosLimpios cu = new CuartosLimpios();
+        cu.ponerBasura();
+        cu.recoger();
+    }
+}
